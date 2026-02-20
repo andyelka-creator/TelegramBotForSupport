3f7da16202d8b5378a5bef6bb11686ef0ccfc2a1
Eliseev Andrey <a@2212971.ru>
2026-02-20 19:12:45 +0300

    Keep asyncpg driver in Alembic SQLAlchemy URL

--- migrations/env.py
+++ migrations/env.py
@@ -17,7 +17,7 @@ from app.db.base import Base
 from app.db.models import *  # noqa: F401,F403
 
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.database_url.replace('+asyncpg', ''))
+config.set_main_option('sqlalchemy.url', settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
